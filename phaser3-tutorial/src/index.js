57fb9143e197deb4f576521e2ccaf025b9503008
Ansel Bridgewater <iamansel@gmail.com>
2018-12-12 22:24:31 -0800

    Added player and animations

--- phaser3-tutorial/src/index.js
+++ phaser3-tutorial/src/index.js
@@ -37,12 +37,38 @@ function create () {
   this.add.image(400, 300, 'sky');
   // refers to physics defined in the config object
   platforms = this.physics.add.staticGroup();
-  // ground platform
+  // ground platform, doubled in size with corresponding refreshed physics
   platforms.create(400, 568, 'ground').setScale(2).refreshBody();
   // elevated platforms
   platforms.create(600, 400, 'ground');
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
+
+  player = this.physics.add.sprite(100, 450, 'dude');
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    // -1 will repeat forever
+    repeat: -1
+  });
+
+  this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'dude', frame: 4 } ],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+  });
 }
 
 function update () {
